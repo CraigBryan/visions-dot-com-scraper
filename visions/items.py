408656686adcb50bfa4bcc04e00199eec90b10b7
Michael Noseworthy <mike.noseworthy@360pi.com>
2015-01-26 14:11:02 -0500

    Remove bad import and fix pep-8
    There is no custom_fields module and NumberField is not used anywhere in
    the code. Removing the import to fix an ImportError exception.
    
    Increase indenting to 4 spaces instead of 2 to conform to PEP-8
    indentation guidelines.

--- visions/items.py
+++ visions/items.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
-from visions.custom_fields import NumberField
+
 
 class ProductItem(scrapy.Item):
-  """Item to hold product information. The product spider uses this item."""
-  category = scrapy.Field()
-  title = scrapy.Field()
-  regular_price = scrapy.Field()
-  sale_price = scrapy.Field()
-  availability = scrapy.Field()
+    """Item to hold product information. The product spider uses this item."""
+    category = scrapy.Field()
+    title = scrapy.Field()
+    regular_price = scrapy.Field()
+    sale_price = scrapy.Field()
+    availability = scrapy.Field()
+
 
 class CategoryItem(scrapy.Item):
-  """Item to hold category name and url. The category spider uses this item"""
-  name = scrapy.Field()
-  url = scrapy.Field()
+    """Item to hold category name and url. The category spider uses this item"""
+    name = scrapy.Field()
+    url = scrapy.Field()
